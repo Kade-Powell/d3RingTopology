bcc0dca18103a74fba65e80591a03ad5af3d5be1
Kade-Powell <47295838+Kade-Powell@users.noreply.github.com>
2020-03-07 09:26:44 -0500

    Update main.js

--- main.js
+++ main.js
@@ -1,2 +1,4 @@
 //This will be the file we write the Java Script in, the import of the d3 library will be in the index html file
+//seems like we'll be using the d3 cluster
+
 alert('main.js working');
