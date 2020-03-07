c50063419f807210c03e56ce269f212586ce2c1a
Kade-Powell <47295838+Kade-Powell@users.noreply.github.com>
2020-03-07 09:51:15 -0500

    Update main.js

--- main.js
+++ main.js
@@ -1,4 +1,17 @@
-//This will be the file we write the Java Script in, the import of the d3 library will be in the index html file
-//seems like we'll be using the d3 cluster
+//seems like we'll be using the d3 tree
 
-alert('main.js working');
+let canvas = d3
+  .select('body')
+  .append('svg')
+  .attr('width', 800)
+  .attr('height', 600)
+  .append('g')
+  .attr('transform', 'translate(50,50)');
+
+let tree = d3.layout.tree().size([400, 400]);
+
+d3.json('apiData.json', function(data) {
+  let nodes = tree.nodes(data);
+  let links = tree.links(nodes);
+  console.log(links);
+});
